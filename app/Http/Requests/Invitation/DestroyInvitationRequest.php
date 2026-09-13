79efadfc4f64b9992159994de6f9d37afd5bf24a
Kevin Muenkel <muenks15@gmail.com>
2026-09-12 20:16:30 -0400

    Updated and debugged all tests

--- app/Http/Requests/Invitation/DestroyInvitationRequest.php
+++ app/Http/Requests/Invitation/DestroyInvitationRequest.php
@@ -14,7 +14,7 @@ class DestroyInvitationRequest extends CattrFormRequest
      */
     public function _authorize(): bool
     {
-        return $this->user()->can('destroy', Invitation::find(request('id')));
+        return $this->user()->can('destroy', Invitation::findOrFail(request('id')));
     }
 
     /**
